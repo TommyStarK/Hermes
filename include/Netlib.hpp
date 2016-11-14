9d590b224b70b9cf4eb80ea73b2d6c238d9827f5
Tommy <thomas.milox@epitech.eu>
2016-11-14 21:06:59 +0100

    [DEPS]	removed boost dependencies.

--- include/Netlib.hpp
+++ include/Netlib.hpp
@@ -2,17 +2,14 @@
 
 #include <iostream>
 #include <string>
-#include "boost/asio.hpp"
-#include "boost/bind/bind.hpp"
 
 namespace netlib {
 
+namespace network {
+
+namespace tcp {
 std::string simple_test_1(const std::string& s) { return std::string(s); }
 
-bool simple_test_2() {
-  boost::asio::io_service service;
-  boost::asio::ip::tcp::socket socket(service);
-  return socket.is_open();
-}
-
+}  // namespace tcp
+}  // namespace network
 }  // namespace netlib
--- tests/main.cpp
+++ tests/main.cpp
@@ -4,12 +4,6 @@
 
 using namespace netlib;
 
-SCENARIO("Test boost headers-only/linked library and Catch") {
-  auto test1 = boost::bind(&simple_test_1, "test");
-  auto test2 = simple_test_2();
-
-  std::cout << test1() << "\n";
-  REQUIRE(test1() == "test");
-  std::cout << std::boolalpha << test2 << "\n";
-  REQUIRE(test2 == false);
+SCENARIO("simple test") {
+  REQUIRE(network::tcp::simple_test_1("toto") == "toto");
 }
