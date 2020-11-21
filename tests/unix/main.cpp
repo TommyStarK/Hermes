9b9f27b2d43a0611288973571515c19cf8de523c
TommyStarK <thomasmilox@gmail.com>
2020-11-21 19:46:59 +0100

    update tests

--- tests/unix/main.cpp
+++ tests/unix/main.cpp
@@ -2,12 +2,12 @@
 #include "hermes.hpp"
 #include "catch.hpp"
 
+using namespace hermes;
 using namespace hermes::network;
 
 void display(const std::string &to_display) {
   std::cout << to_display + " [\x1b[32;1mOK\x1b[0m]\n";
 }
-
 
 //
 // Tests: TCP socket.
@@ -38,3 +38,346 @@ TEST_CASE("TCP socket tests", "[network][tcp][socket][default]") {
     display("[TCP socket] Testing default behavior");
   }
 }
+
+TEST_CASE("TCP socket tests server operations",
+          "[network][tcp][socket][operations][server]") {
+  SECTION("Should assign a name to the socket") {
+    tcp::socket socket1;
+    tcp::socket socket2;
+
+    CHECK(socket1 == socket2);
+    REQUIRE_NOTHROW(socket1.bind("127.0.0.1", 27017));
+
+    THEN("tryng to bind the socket again should diplay a warning message") {
+      CHECK_NOTHROW(socket1.bind("127.0.0.1", 27017));
+    }
+
+    REQUIRE(socket1.fd() != -1);
+    CHECK(socket1.host() == "127.0.0.1");
+    CHECK(socket1.port() == 27017);
+    CHECK(socket1.bound());
+    CHECK(!(socket1 == socket2));
+
+    REQUIRE_NOTHROW(socket1.close());
+    THEN("Should be equal to socket2") { CHECK(socket1 == socket2); }
+  }
+
+  SECTION("Should mark the socket as passive") {
+    tcp::socket socket;
+
+    REQUIRE_THROWS_AS(socket.listen(10), std::logic_error);
+    REQUIRE_NOTHROW(socket.bind("127.0.0.1", 27017));
+    CHECK(socket.bound());
+    REQUIRE_NOTHROW(socket.listen(50));
+
+    THEN("Trying to connect a socket marked as passive should throw an error") {
+      REQUIRE_THROWS_AS(socket.connect("127.0.0.1", 27017), std::logic_error);
+    }
+
+    REQUIRE_NOTHROW(socket.close());
+
+    THEN("Should reset the socket") {
+      CHECK(socket.fd() == -1);
+      CHECK(!socket.bound());
+    }
+  }
+
+  SECTION("Should accept a new connection") {
+    std::thread server([] {
+      tcp::socket socket;
+
+      REQUIRE_NOTHROW(socket.bind("127.0.0.1", 27017));
+      REQUIRE_NOTHROW(socket.listen(10));
+      auto client = std::make_shared<tcp::socket>(socket.accept());
+      CHECK(!(socket == *client));
+      REQUIRE_NOTHROW(socket.close());
+    });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    std::thread client([] {
+      tcp::socket socket;
+
+      REQUIRE_NOTHROW(socket.connect("127.0.0.1", 27017));
+      REQUIRE_NOTHROW(socket.close());
+    });
+
+    REQUIRE_NOTHROW(server.join());
+    REQUIRE_NOTHROW(client.join());
+  }
+}
+
+TEST_CASE("TCP socket tests client operations",
+          "[network][tcp][socket][operations][client]") {
+  SECTION("Should correctly send/receive data") {
+    std::thread server([] {
+      tcp::socket socket;
+
+      REQUIRE_NOTHROW(socket.bind("127.0.0.1", 27017));
+      REQUIRE_NOTHROW(socket.listen(10));
+      auto client = std::make_shared<tcp::socket>(socket.accept());
+      std::string data(client->receive().data());
+      REQUIRE(data.size() == 13);
+      REQUIRE(data == "Hello world!\n");
+      REQUIRE_NOTHROW(socket.close());
+    });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    std::thread client([] {
+      tcp::socket socket;
+
+      REQUIRE_NOTHROW(socket.connect("127.0.01", 27017));
+      auto bytes_sent = socket.send("Hello world!\n");
+      REQUIRE(bytes_sent == 13);
+      REQUIRE_NOTHROW(socket.close());
+    });
+
+    REQUIRE_NOTHROW(server.join());
+    REQUIRE_NOTHROW(client.join());
+  }
+}
+
+// //
+// // Tests: TCP server/client.
+// //
+// TEST_CASE("TCP server/client tests", "[network][tcp][server][client]") {
+//   SECTION("Default TCP server should have default behaviour") {
+//     tcp::server server;
+
+//     CHECK(!server.is_running());
+//     CHECK(server.get_socket().fd() == -1);
+//     CHECK(!server.get_socket().bound());
+//     CHECK_THROWS_AS(server.run("127.0.0.1", 27017), std::invalid_argument);
+//   }
+
+//   SECTION("Default TCP client should have default behaviour") {
+//     tcp::client client;
+// 		std::vector<char> data;
+
+//     CHECK(!client.is_connected());
+//     CHECK(client.get_socket().fd() == -1);
+//     CHECK(!client.get_socket().bound());
+//     CHECK_THROWS_AS(client.async_write(data, nullptr), std::logic_error);
+//     CHECK_THROWS_AS(client.async_read(1024, nullptr), std::logic_error);
+//   }
+
+//   SECTION("Should correctly send and receive data") {
+//     tcp::server server;
+//     tcp::client client;
+
+//     std::thread thread_server([&server] {
+
+//       server.on_connection([](const std::shared_ptr<tcp::client> &client) {
+//         client->async_read(1024, [](bool success, std::vector<char> data) {
+//           if (success) std::cout << data.data();
+//         });
+//       });
+
+//       REQUIRE_NOTHROW(server.run("127.0.0.1", 27017));
+//       std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+//       REQUIRE_THROWS_AS(server.run("127.0.0.1", 27017), std::logic_error);
+//     });
+
+//     std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+//     std::thread thread_client([&client] {
+
+//       REQUIRE_NOTHROW(client.connect("127.0.0.1", 27017));
+
+// 			std::string s = "Hello world!\n";
+//   	  std::vector<char> data(s.begin(), s.end());
+//       client.async_write(data, [](bool success, std::size_t sent) {
+//         if (success) REQUIRE(sent == 13);
+//       });
+
+//       REQUIRE_THROWS_AS(client.connect("127.0.0.1", 27017), std::logic_error);
+//     });
+
+//     REQUIRE_NOTHROW(thread_server.join());
+//     REQUIRE_NOTHROW(thread_client.join());
+//   }
+// }
+
+//
+// Tests: UDP socket.
+//
+TEST_CASE("UDP socket tests", "[network][udp][socket][default]") {
+  SECTION("Default UDP socket should have default data") {
+    udp::socket socket1;
+    udp::socket socket2;
+
+    CHECK(socket1.fd() == -1);
+    CHECK(socket1.host() == "");
+    CHECK(socket1.port() == 0);
+    CHECK(socket1.bound() == false);
+
+    THEN("The two default UDP sockets should be equal") {
+      CHECK(socket1 == socket2);
+    }
+  }
+
+  SECTION("Default UDP socket should have default behaviour") {
+    udp::socket socket;
+    std::vector<char> buffer;
+
+    REQUIRE_THROWS_AS((void)socket.sendto(""), std::logic_error);
+    REQUIRE_THROWS_AS((void)socket.broadcast(""), std::logic_error);
+    REQUIRE_THROWS_AS((void)socket.recvfrom(buffer), std::logic_error);
+  }
+
+  SECTION("Should initialize a default datagram socket") {
+    udp::socket socket1;
+    udp::socket socket2;
+
+    REQUIRE_NOTHROW(socket1.init_datagram_socket("127.0.0.1", 27017, false));
+
+    THEN("fd should not be equal to -1, port shoudl be equal to 27017") {
+      CHECK(socket1.fd() != -1);
+      CHECK(socket1.host() == "127.0.0.1");
+      CHECK(socket1.port() == 27017);
+      CHECK(socket1.bound() == false);
+      CHECK((socket1 == socket2) == false);
+    }
+
+    REQUIRE_NOTHROW(socket1.close());
+    THEN("fd should be equal to -1") {
+      CHECK(socket1.fd() == -1);
+      CHECK(socket1 == socket2);
+    }
+  }
+
+  SECTION(
+      "Should initialize a default datagram socket with broadcasting mode "
+      "enabled") {
+    udp::socket socket1;
+    udp::socket socket2;
+
+    REQUIRE_NOTHROW(socket1.init_datagram_socket("127.0.0.1", 27017, true));
+
+    THEN("fd should not be equal to -1, port shoudl be equal to 27017") {
+      CHECK(socket1.fd() != -1);
+      CHECK(socket1.host() == "127.0.0.1");
+      CHECK(socket1.port() == 27017);
+      CHECK(socket1.bound() == false);
+      CHECK((socket1 == socket2) == false);
+    }
+
+    REQUIRE_NOTHROW(socket1.close());
+    THEN("fd should be equal to -1") {
+      CHECK(socket1.fd() == -1);
+      CHECK(socket1 == socket2);
+    }
+  }
+
+  SECTION("Should initialize and bind a default datagram socket") {
+    udp::socket socket1;
+    udp::socket socket2;
+
+    REQUIRE_NOTHROW(socket1.bind("127.0.0.1", 27017));
+
+    THEN("fd should not be equal to -1, port shoudl be equal to 27017") {
+      CHECK(socket1.fd() != -1);
+      CHECK(socket1.host() == "127.0.0.1");
+      CHECK(socket1.port() == 27017);
+      CHECK(socket1.bound());
+      CHECK((socket1 == socket2) == false);
+    }
+
+    REQUIRE_NOTHROW(socket1.close());
+    THEN("fd should be equal to -1") {
+      CHECK(socket1.fd() == -1);
+      CHECK(socket1 == socket2);
+    }
+  }
+}
+
+TEST_CASE("UDP socket tests sending/broadcasting/receiving data.",
+          "[network][udp][socket][operations][server][client]") {
+  SECTION("Should correctly send/receive data") {
+    std::thread server([] {
+      udp::socket socket;
+      std::vector<char> buffer;
+
+      buffer.reserve(internal::BUFFER_SIZE);
+      REQUIRE_NOTHROW(socket.bind("127.0.0.1", 27017));
+      REQUIRE(socket.bound());
+      auto result = socket.recvfrom(buffer);
+      REQUIRE(result == 13);
+      REQUIRE_NOTHROW(socket.close());
+    });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    std::thread client([] {
+      udp::socket socket;
+
+      REQUIRE_NOTHROW(socket.init_datagram_socket("127.0.01", 27017, true));
+      auto bytes_sent = socket.broadcast("Hello world!\n");
+      REQUIRE(bytes_sent == 13);
+      REQUIRE_NOTHROW(socket.close());
+    });
+
+    REQUIRE_NOTHROW(server.join());
+    REQUIRE_NOTHROW(client.join());
+  }
+}
+
+// //
+// // Tests: UDP server/client.
+// //
+// TEST_CASE("UDP server/client tests", "[network][udp][server][client]") {
+//   // SECTION("Default UDP server should have default behaviour") {
+//   //   udp::server server;
+
+//   //   CHECK(!server.is_running());
+//   //   CHECK(server.get_socket().fd() == -1);
+//   //   CHECK(!server.get_socket().bound());
+//   //   REQUIRE_THROWS_AS(server.async_recvfrom(nullptr), std::logic_error);
+//   // }
+
+//   // SECTION("Default UDP client should have default behaviour") {
+//   //   udp::client client;
+
+//   //   CHECK(client.get_socket().fd() == -1);
+//   //   CHECK(!client.get_socket().bound());
+//   //   CHECK(!client.broadcast_mode_enabled());
+//   // }
+
+//   SECTION("Should correctly send and receive data") {
+//     udp::server server;
+//     udp::client client;
+
+//     std::thread thread_server([&server] {
+
+//       REQUIRE_NOTHROW(server.bind("127.0.0.1", 27017));
+
+//       server.async_recvfrom([](std::vector<char> data, int bytes_sent) {
+//         if (bytes_sent) {
+//           REQUIRE(bytes_sent == 13);
+//           std::cout << data.data();
+//         }
+//       });
+
+//       std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+//       REQUIRE_THROWS_AS(server.bind("127.0.0.1", 27017), std::logic_error);
+//     });
+
+//     std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+//     std::thread thread_client([&client] {
+
+//       REQUIRE_NOTHROW(client.init("127.0.0.1", 27017, true));
+
+//       client.async_broadcast("Hello world!\n", [](int bytes_sent) {
+//         if (bytes_sent) {
+//           REQUIRE(bytes_sent == 13);
+//         }
+//       });
+
+//     });
+
+//     REQUIRE_NOTHROW(thread_server.join());
+//     REQUIRE_NOTHROW(thread_client.join());
+//   }
+// }
